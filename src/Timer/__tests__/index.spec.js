946874fe1bf7df0808499c59ecc5dc1863d51d6a
lius <liuswin@163.com>
2021-03-05 11:21:11 +0800

    test: add case

---
+++ src/Timer/__tests__/index.spec.js
@@ -0,0 +1,10 @@
+
+describe('toolbar base test', () => {
+  it.skip('test render', () => {
+    const wrapper = render(
+      <div></div>
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
